eb0b4c387439ca3252ae230a7bf3a55fab1aea17
zhangxiawei <zhangxiawei@readtv.cn>
2017-04-10 14:15:00 +0800

    第15回:附加课:使用Proxy模拟PHP的__get魔术函数

--- core/index.js
+++ core/index.js
@@ -16,12 +16,41 @@ Function.prototype.extends = function (base) {
 
 var SportsNews = function () {
     this.version = "1.0";
+    var prefix="【程序员在囧途】";
+    var title = "新闻标题";
     this.display = function () {
         console.log("子类的方法display")
+    };
+    this.getTitle=()=>{
+        return prefix+title;
+    };
+    this.setTitle=(newTitle)=>{
+        //ajax
+        title=newTitle;
     }
-};
-SportNews.extends(BaseNews);
 
-var sn = new SportNews();
-sn.father.display();
-sn.display();
+};
+//SportsNews.extends(BaseNews);
+
+var sn = new SportsNews();
+
+var proxy = new Proxy(sn,{
+    get(target, name)
+    {
+        if (name in target) {
+            return target[name];
+        }
+        else {
+            return "none";
+        }
+    },
+    set(target, name, value)
+    {
+        if(name=="version") {
+            console.log("不要胡搞")
+        }
+    }
+});
+console.log(proxy.version);
+console.log(proxy.age);
+proxy.version=2;
