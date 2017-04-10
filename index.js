90795fc78b96e783998472a649f482d4c97c00fc
zhangxiawei <zhangxiawei@readtv.cn>
2017-04-10 16:52:29 +0800

    第16回:使用node.js+babel提升我们的档次、import/export语法

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by xiawei on 2017/4/10.
+ */
+import bbb,{BaseNews,version} from './BaseNews.js';
+
+var news = new BaseNews();
+news.display();
+console.log(bbb);
+console.log(version);
