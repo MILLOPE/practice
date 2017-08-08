3e26a9a72db807bbbcbc7ec2884383ef655b2d50
张夏玮 <zhangxiawei@readtv.cn>
2017-08-08 21:55:15 +0800

    lession 5-1:父子组件之children、与后端交互显示新闻列表、React路由入门初步

--- pro/src/js/index.js
+++ pro/src/js/index.js
@@ -1,8 +1,31 @@
-import ReactDOM from 'react-dom';
-import News from "./../components/News";
+import React from "react";
+import ReactDOM from "react-dom";
 
+class Age extends React.Component
+{
+    render()
+    {
+        return <div>
+            我的年龄是{this.props.myage}
+        </div>
+    }
+}
+
+class Me extends React.Component
+{
+    render()
+    {
+        console.log(this.props.children);
+        return <div>
+            {this.props.children}
+        </div>
+    }
+}
 ReactDOM.render(
-    <News newsid="101"/>,
+    <Me myname="shenyi">
+        <div>我的名字是shenyi</div>
+        <Age myage="20"/>
+    </Me>,
     document.getElementById("root")
 );
 
