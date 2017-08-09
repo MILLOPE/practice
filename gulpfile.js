3d602cb13df5dabcfbbe2d02114b5660cb27fbb7
张夏玮 <zhangxiawei@readtv.cn>
2017-08-09 22:25:40 +0800

    lession 8:gulp篇:速玩JS文件合并和压缩

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gp=require("gulp");
+var gulp_concat=require("gulp-concat");
+var gulp_uglify=require("gulp-uglify");
 
 gp.task("shenyi",function(){
 
     gp.src(["*.js","!gulpfile.js"]).pipe(gp.dest("./build/js"));
-    gp.src(["*.css"]).pipe(gp.dest("./build/css"))
+    gp.src(["*.css"]).pipe(gp.dest("./build/css"));
+
+    gp.src(["1.js","2.js"])
+        .pipe(gulp_concat("main.js"))//合并
+        .pipe(gulp_uglify())//压缩
+        .pipe(gp.dest("./build/js"));
 });
