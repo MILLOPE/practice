82f5be4ff97e5ea9d6b3418471ed3ff3b17d02b1
张夏玮 <zhangxiawei@readtv.cn>
2017-09-03 22:40:34 +0800

    lession 12-2:直接上手组件化开发:寻求"刺激"的”学习方法(components组件化)

---
+++ vuepro/src/components/myage.vue
@@ -0,0 +1,6 @@
+export default {
+    template:'<div id="me">我的年龄是{{age}}</div>',
+    data: ()=>{
+        return {age: '18'};
+    }
+};
--- vuepro/src/jssrc/index.js
+++ vuepro/src/jssrc/index.js
@@ -1,15 +1,9 @@
 import Vue from "vue";
 
-let me_name={
-    template:'<div id="me">我的名字是{{name}}</div>',
-    data: ()=>{
-        return {name: 'Hello Vue!'};
-    }
-};
+import me_name from "../components/myage.vue";
 
 let param = {
     el: '.container',
     components:{'myname':me_name},
-
 };
 new Vue(param);
