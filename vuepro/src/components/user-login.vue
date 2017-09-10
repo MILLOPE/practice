fd30aa5f95aea40f3c676bf9b1f787791dc64952
张夏玮 <zhangxiawei@readtv.cn>
2017-09-10 23:24:29 +0800

    lession 19:带着实例继续学习(5):模拟搜索框动态提示效果之计算属性

--- vuepro/src/components/user-login.vue
+++ vuepro/src/components/user-login.vue
@@ -9,9 +9,10 @@
         <h2 class="text-center" v-if="isadmin">Admin Login</h2>
         <h2 class="text-center" v-else>User Login</h2>
         <form class="form-horizontal" role="form">
-            <user-name ref="uname" v-on:childChange="setValue" placeholder="input username"></user-name>
-            <user-pass ref="upass" v-on:childChange="setValue" placeholder="input userpass"></user-pass>
+            <user-name ref="uname" placeholder="input username"></user-name>
+            <user-pass ref="upass" placeholder="input userpass"></user-pass>
             <user-area v-on:childChange="setValue"></user-area>
+            <user-favs></user-favs>
             <user-submit></user-submit>
             <input type="button" value="test" v-on:click="show">
         </form>
@@ -22,7 +23,13 @@
     import userpass from './user/user-pass.vue';
     import usersubmit from './user/user-submit.vue';
     import userarea from './user/user-area.vue';
+    import userfavs from './user/user-favs.vue';
     export default {
+        mounted(){
+            this.$children.forEach(function (cc) {
+                cc.$on("childChange",this.setValue)
+            }.bind(this))
+        },
         data () {
             return {
                 username:'',
@@ -46,14 +53,16 @@
             },
             show(){
                 //this.isadmin=!this.isadmin;
-                alert(this.userarea);
+                //alert(this.userarea);
+                console.log(this.$children[3].inputText);
             }
         },
         components: {
             'user-name': username,
             'user-pass': userpass,
             'user-submit': usersubmit,
-            'user-area': userarea
+            'user-area': userarea,
+            'user-favs': userfavs
         }
     }
 </script>
---
+++ vuepro/src/components/user/user-favs.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="row">
+        <div class="form-group">
+            <label class="col-sm-2 control-label">your favourite:</label>
+            <div class="col-sm-10">
+                <input type="text" v-model="inputText" class="form-control">
+                <table v-if="isShow()" class="table">
+                    <thead>
+                        <tr>
+                            <th>
+                                type 1
+                            </th>
+                            <th>
+                                type 2
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="fav in getFavs">
+                            <td>{{ fav.class1 }}</td>
+                            <td>{{ fav.class2 }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                favs:[
+                    {'class1':'web','class2':'js'},
+                    {'class1':'pro','class2':'java'}
+                ],
+                inputText:''
+            }
+        },
+        methods:{
+            isShow(){
+                return !(this.inputText=='');
+            }
+        },
+        computed:{
+            getFavs(){
+                return this.favs.filter(function(abc){
+                    return (abc.class2.indexOf(this.inputText)>=0);
+                }.bind(this))
+            }
+
+        }
+    }
+</script>
