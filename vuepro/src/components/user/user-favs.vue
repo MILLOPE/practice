a0de6c706baf8144e5eb09f28130be9fffd7949b
张夏玮 <zhangxiawei@readtv.cn>
2017-09-11 17:03:03 +0800

    lession 20:模拟搜索框动态提示效果(课后作业代码）

--- vuepro/src/components/user/user-favs.vue
+++ vuepro/src/components/user/user-favs.vue
@@ -18,7 +18,7 @@
                     <tbody>
                         <tr v-for="fav in getFavs">
                             <td>{{ fav.class1 }}</td>
-                            <td>{{ fav.class2 }}</td>
+                            <td><a v-for="code in filterClass2(fav.class2)">{{ code }}</a></td>
                         </tr>
                     </tbody>
                 </table>
@@ -26,13 +26,18 @@
         </div>
     </div>
 </template>
+<style scoped>
+    .table td a {
+        width: 25px;margin-left: 5px;
+    }
+</style>
 <script>
     export default {
         data(){
             return {
                 favs:[
-                    {'class1':'web','class2':'js'},
-                    {'class1':'pro','class2':'java'}
+                    {'class1':'web','class2':['js','html','css','jssdk']},
+                    {'class1':'pro','class2':['java']}
                 ],
                 inputText:''
             }
@@ -40,13 +45,37 @@
         methods:{
             isShow(){
                 return !(this.inputText=='');
+            },
+            filterClass2(class2){
+                return class2.filter(bbb=>{
+                    return bbb.indexOf(this.inputText)>=0;
+                })
             }
         },
         computed:{
             getFavs(){
-                return this.favs.filter(function(abc){
+                /*return this.favs.filter(function(abc){
                     return (abc.class2.indexOf(this.inputText)>=0);
-                }.bind(this))
+                }.bind(this))*/
+
+                /*let arr = [];
+                this.favs.forEach((a)=>{
+                    this.inputText
+                    var obj={};
+                    obj.class1=a.class1;
+                    obj.class2=a.class2.filter((b=>{
+                        return b.indexOf(this.inputText)>=0;
+                    }));
+                    if(obj.class2.length>0) arr.push(obj);
+                })
+                console.log(arr);
+                return arr;*/
+
+                return this.favs.filter(abc=>{
+                    return abc.class2.filter((code)=>{
+                        return code.indexOf(this.inputText)>=0
+                    }).length>0;
+                })
             }
 
         }
