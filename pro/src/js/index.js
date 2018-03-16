676f61e925f84f1f0630a6a1a5f54e3945c5cf9e
xiawei <xiaweiss@foxmail.com>
2018-03-16 16:01:55 +0800

    lession 9-1:Redux和React组件的异步交互、中间件Redux-thunk的使用、案例:增加点赞数

--- pro/src/js/index.js
+++ pro/src/js/index.js
@@ -8,26 +8,22 @@ import InfoReduce from '../redux/InfoReduce';
 let store = createStore(InfoReduce);
 
 class InfoDetail extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      infoData: store.getState()
-    }
+  componentWillMount() {
+    this.props.Store.subscribe(()=>{
+      this.forceUpdate();
+    });
   }
 
   addClick() {
-    store.dispatch({
+    this.props.Store.dispatch({
       type: "INFO_ADDCLICK",
-    })
-    this.setState({
-      infoData: store.getState()
     })
   }
 
   render() {
     return <div>
-      <h2>新闻标题：{this.state.infoData.title}</h2>
-      <span>点击量：{this.state.infoData.clicknum}</span>
+      <h2>新闻标题：{this.props.Store.getState().title}</h2>
+      <span>点击量：{this.props.Store.getState().clicknum}</span>
       <div>
         <button onClick={this.addClick.bind(this)}>修改点击量</button>
       </div>
@@ -37,6 +33,6 @@ class InfoDetail extends React.Component {
 
 ReactDOM.render(
     // <MyRouter />
-    <InfoDetail />
+    <InfoDetail Store={store} />
     ,document.getElementById("root")
 );
